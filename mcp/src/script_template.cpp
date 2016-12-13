c1299d57d14ed9b85d45a76f7058bc10676064b0
mcjurij <sun.sucks@gmail.com>
2016-12-13 04:36:36 +0100

    class can be made private

--- mcp/src/script_template.cpp
+++ mcp/src/script_template.cpp
@@ -10,7 +10,37 @@
 
 using namespace std;
 
+
 static map<string,string> fileMap;
+
+class ScriptTemplate
+{
+public:
+    ScriptTemplate( const std::string &fn )
+        : fn(fn)
+    {}
+
+    bool readTemplate();
+
+    void replace( const std::map<std::string,std::string> &replMap);
+
+    std::string write( const std::string &scriptfn, int file_id);
+
+private:
+    bool hasNextChar() const
+    { return pos < templContent.length(); }
+    
+    char consume();
+    
+    char getc() const
+    { return templContent[pos]; }
+    
+private:
+    std::string fn;
+    std::string templContent;  // we slurp the file in
+    std::string newContent;
+    size_t pos;
+};
 
 
 static string findTemplate( const string &fn )
