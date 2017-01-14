b021291eba0b230416e61762a413abf8cadda1d8
mc_jurij <mc_jurij@kommunismus-rockt.de>
2017-01-14 19:53:50 +0100

    made ncurses example more interesting

--- ncurses_example/hello/src/hello_ncurses.c
+++ ncurses_example/hello/src/hello_ncurses.c
@@ -1,16 +1,126 @@
-/* From
- * http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
- */
+#include <stdlib.h>
+#include <stdio.h>
+#include <curses.h>
+#include <string.h>
 
-#include <ncurses.h>
+#define WIDTH 30
+#define HEIGHT 10 
+
+int startx = 0;
+int starty = 0;
+
+const char *choices[] = { 
+    "Hello, world!",
+    "Choice #2",
+    "Choice #3",
+    "Choice #4",
+    "Exit",
+};
+
+int n_choices = sizeof(choices) / sizeof(char *);
+void print_menu(WINDOW *menu_win, int highlight);
 
 int main()
-{   
-    initscr();          /* Start curses mode      */
-    printw("Hello World !!!");  /* Print Hello World      */
-    refresh();          /* Print it on to the real screen */
-    getch();            /* Wait for user input    */
-    endwin();           /* End curses mode        */
+{
+    WINDOW *menu_win;
+    int highlight = 1;
+    int choice = 0;
+    int c;
+
+    initscr();
+    clear();
+    noecho();
+    cbreak();   /* Line buffering disabled. pass on everything */
+
+    int mx,my;
+    getmaxyx(stdscr, my, mx);
+    // keypad( stdscr, TRUE);
+
+    startx = (mx - WIDTH) / 2;
+    starty = (my - HEIGHT) / 2;
+        
+    menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+    keypad(menu_win, TRUE);
+    
+    mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+    mvhline( 1, 0, ACS_HLINE , mx);
+    refresh();
+    
+    print_menu(menu_win, highlight);
+    
+    while(1)
+    {
+        c = wgetch(menu_win);
+        switch(c)
+        {
+        case KEY_UP:
+            if(highlight == 1)
+                highlight = n_choices;
+            else
+                highlight--;
+            mvprintw( 0, 0, "Choice %d with choice string %s\n", highlight, choices[highlight - 1]);
+            refresh();
+            break;
+        case KEY_DOWN:
+            if(highlight == n_choices)
+                highlight = 1;
+            else 
+                highlight++;
+            mvprintw( 0, 0, "Choice %d with choice string %s\n", highlight, choices[highlight - 1]);
+            refresh();
+            break;
+        case KEY_LEFT:
+        case KEY_RIGHT:
+            break;
+        case 10:
+        case KEY_ENTER:
+            choice = highlight;
+            break;
+        default:
+            mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+            refresh();
+            break;
+        }
+        
+        print_menu(menu_win, highlight);
+        if(choice != 0) /* User did a choice come out of the infinite loop */
+            break;
+    }
+    
+    wclear( menu_win );
+    wrefresh( menu_win );
+    delwin( menu_win );
+    refresh();
+    mvprintw(24, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+    // clrtoeol();
+    refresh();
+    
+    getch();
+    endwin();
     
     return 0;
+}
+
+
+void print_menu(WINDOW *menu_win, int highlight)
+{
+    int x, y, i;    
+    
+    x = 2;
+    y = 2;
+    box(menu_win, 0, 0);
+    
+    for(i = 0; i < n_choices; ++i)
+    {
+        if(highlight == i + 1) /* High light the present choice */
+        {
+            wattron(menu_win, A_REVERSE); 
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+            wattroff(menu_win, A_REVERSE);
+        }
+        else
+            mvwprintw(menu_win, y, x, "%s", choices[i]);
+        ++y;
+    }
+    wrefresh(menu_win);
 }
